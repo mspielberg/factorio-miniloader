1554ac57fccea9b49b3c5e2079cadaa0baed4ac7
mspielberg <9729801+mspielberg@users.noreply.github.com>
2017-12-01 12:32:43 -0800

    with chest (nonworking)

--- control.lua
+++ control.lua
@@ -1,24 +1,60 @@
 require "util"
 
+--[[
+	belt_to_ground_type = "input"
+	+------------------+
+	|                  |
+	|        P         |
+	|                  |
+	|                  |    |
+	|        C         |    | chest dir
+	|                  |    |
+	|                  |    v
+	|                  |
+	+------------------+
+	   D            D
+
+	belt_to_ground_type = "output"
+	+------------------+
+	|                  |
+	|  D            D  |
+	|                  |
+	|                  |    |
+	|        C         |    | chest dir
+	|                  |    |
+	|                  |    v
+	|                  |
+	+------------------+
+	         P
+
+	C: miniloader-chest
+	D: drop positions
+	P: pickup position
+]]
+
 -- Constants
 
 -- Utility Functions
 
-local function moveposition(position, direction, distance)
+local function moveposition(position, offset)
+	return {x=position.x + offset.x, y=position.y + offset.y}
+end
+
+local function offset(direction, longitudinal, orthogonal)
 	if direction == defines.direction.north then
-		return {x=position.x, y=position.y - distance}
+		return {x=orthogonal, y=-longitudinal}
 	end
 
 	if direction == defines.direction.south then
-		return {x=position.x, y=position.y + distance}
+		return {x=-orthogonal, y=longitudinal}
 	end
 
 	if direction == defines.direction.east then
-		return {x=position.x + distance, y=position.y}
+		return {x=longitudinal, y=orthogonal}
 	end
 
 	if direction == defines.direction.west then
-		return {x=position.x - distance, y=position.y}
+		return {x=-longitudinal, y=-orthogonal}
 	end
 end
 
@@ -32,75 +68,98 @@ end
 local function orthogonal_direction(direction)
 	if direction == defines.direction.west then
 		return defines.direction.north
-	else
-		return direction + 2
 	end
+	return direction + 2
 end
 
-local function is_railloader(entity)
-	return string.find(entity.name, "^railloader") ~= nil
+local function is_miniloader(entity)
+	return string.find(entity.name, "-miniloader$") ~= nil
 end
 
 local function chest_direction(entity)
-	local dir = entity.direction
 	if entity.belt_to_ground_type == "output" then
-		return util.oppositedirection(dir)
+		return util.oppositedirection(entity.direction)
 	end
-	return dir
+	return entity.direction
+end
+
+local function pickup_position(entity)
+	if entity.belt_to_ground_type == "output" then
+		return moveposition(entity.position, offset(opposite_direction(entity.direction), 0.75, 0))
+	end
+	return moveposition(entity.position, offset(entity.direction, -0.25, 0))
+end
+
+local function drop_positions(entity)
+	if entity.belt_to_ground_type == "output" then
+		local chest_dir = opposite_direction(entity.direction)
+		local p1 = moveposition(entity.position, offset(chest_dir, -1, 0.25))
+		local p2 = moveposition(p1, offset(chest_dir, 0, -0.5))
+		return {p1, p2}
+	end
+	local chest_dir = entity.direction
+	local p1 = moveposition(entity.position, offset(chest_dir, 0.75, 0.25))
+	local p2 = moveposition(p1, offset(chest_dir, 0, -0.5))
+	return {p1, p2}
 end
 
 local function get_loader_inserters(entity)
 	return entity.surface.find_entities_filtered{
 		position = entity.position,
-		name = "railloader-inserter"
+		name = entity.name .. "-inserter"
 	}
 end
 
 local function update_inserters(entity)
 	local inserters = get_loader_inserters(entity)
 	local chest_dir = chest_direction(entity)
-	local belt_base_position = moveposition(entity.position, chest_dir, 0.25)
-	local ortho = orthogonal_direction(entity.direction)
-	for i = 1, 2 do
-		local inserter = inserters[i]
-		local ortho_offset = (i == 1) and 0.25 or -0.25
-		local longi_offset = 0.5
-		local belt_position = moveposition(belt_base_position, ortho, ortho_offset)
-		local chest_position = moveposition(belt_position, chest_dir, longi_offset)
-		if entity.belt_to_ground_type == "output" then
-			inserter.pickup_position = chest_position
-			inserter.drop_position = belt_position
-		else
-			inserter.pickup_position = belt_position
-			inserter.drop_position = chest_position
-		end
-		inserter.direction = inserter.direction
+	local pickup = pickup_position(entity, chest_dir)
+	game.print("pickup = " .. serpent.line(pickup))
+	local drop = drop_positions(entity, chest_dir)
+	game.print("drop = " .. serpent.line(drop))
+
+	-- drop inserters
+	inserters[1].pickup_position = entity.position
+	inserters[1].drop_position = drop[1]
+	inserters[2].pickup_position = entity.position
+	inserters[2].drop_position = drop[2]
+	-- pickup inserter
+	inserters[3].pickup_position = pickup
+	inserters[3].drop_position = entity.position
+
+	for i = 1, 3 do
+		inserters[i].direction = inserters[i].direction
 	end
 end
 
 -- Event Handlers
 
 local function on_init()
-	local force = game.create_force("railloader")
+	local force = game.create_force("miniloader")
 	force.stack_inserter_capacity_bonus = 24
-	-- allow railloader force to access chests belonging to players
+	-- allow miniloader force to access chests belonging to players
 	game.forces["player"].set_friend(force, true)
-	-- allow players to see power icons on railloader inserters
+	-- allow players to see power icons on miniloader inserters
 	force.set_friend(game.forces["player"], true)
 end
 
 local function on_built(event)
 	local entity = event.created_entity
-	if not is_railloader(entity) then
+	if not is_miniloader(entity) then
 		return
 	end
-	local ortho = orthogonal_direction(entity.direction)
 	local surface = entity.surface
-	for i = 1, 2 do
+	local chest = surface.create_entity{
+		name = "miniloader-chest",
+		position = entity.position,
+		force = "miniloader",
+	}
+	chest.destructible = false
+	for i = 1, 3 do
 		local inserter = surface.create_entity{
-			name = "railloader-inserter",
+			name = entity.name .. "-inserter",
 			position = entity.position,
-			force = "railloader",
+			force = "miniloader",
 		}
 		inserter.destructible = false
 	end
@@ -109,7 +168,7 @@ end
 
 local function on_rotated(event)
 	local entity = event.entity
-	if not is_railloader(entity) then
+	if not is_miniloader(entity) then
 		return
 	end
 	update_inserters(entity)
@@ -117,16 +176,27 @@ end
 
 local function on_mined(event)
 	local entity = event.entity
-	if not is_railloader(entity) then
+	if not is_miniloader(entity) then
 		return
 	end
-	for _, inserter in pairs(get_loader_inserters(entity)) do
+
+	local inserters = get_loader_inserters(entity)
+	for i=1, #inserters do
 		-- return items to player / robot if mined
 		if event.buffer then
-			event.buffer.insert(inserter.held_stack)
+			event.buffer.insert(inserters[i].held_stack)
 		end
-		inserter.destroy()
+		inserters[i].destroy()
 	end
+
+	local chest = entity.surface.find_entity("miniloader-chest", entity.position)
+	if event.buffer then
+		local chest_inventory = chest.get_inventory(defines.inventory.chest)
+		if not chest_inventory.is_empty() then
+			event.buffer.insert(chest_inventory[1])
+		end
+	end
+	chest.destroy()
 end
 
 script.on_init(on_init)
--- data.lua
+++ data.lua
@@ -6,36 +6,58 @@ local empty_sheet = {
 	width = 0,
 	height = 0,
 }
-local loader_inserter = {
-	type = "inserter",
-	name = "railloader-inserter",
-	flags = {"placeable-off-grid"},
-	stack = true,
-	allow_custom_vectors = true,
-	energy_per_movement = 1,
-	energy_per_rotation = 1,
-	extension_speed = 1.0,
-	rotation_speed = 1.0,
-	collision_box = {{-0.15, -0.15}, {0.15, 0.15}},
-	selection_box = {{-0.0, -0.0}, {0.0, 0.0}},
-	pickup_position = {0, 1.0},
-	insert_position = {0, 2.0},
-	platform_picture = { sheet = empty_sheet },
-	hand_base_picture = empty_sheet,
-	hand_open_picture = empty_sheet,
-	hand_closed_picture = empty_sheet,
-	energy_source = {
-		type = "electric",
-		usage_priority = "secondary-input",
-	}
-}
-data.raw.inserter["railloader-inserter"] = loader_inserter
 
-local function create_railloader(base_entity, tech_prereqs)
-	local name = "railloader-" .. base_entity.name
+data.raw.container["miniloader-chest"] = {
+	type = "container",
+	name = "miniloader-chest",
+	collision_box = {{-0.2, -0.2}, {0.2, 0.2}},
+	selection_box = {{-0.0, -0.0}, {0.0, 0.0}},
+	fast_replaceable_group = "container",
+	inventory_size = 1,
+	picture = empty_sheet,
+}
+
+local function create_inserter(base_entity)
+	local name = base_entity.name .. "-miniloader-inserter"
+	local loader_inserter = {
+		type = "inserter",
+		name = name,
+		icon = base_entity.icon,
+		flags = {"placeable-off-grid"},
+		max_health = base_entity.max_health,
+		stack = false,
+		allow_custom_vectors = true,
+		energy_per_movement = 20000,
+		energy_per_rotation = 20000,
+		energy_source = {
+			type = "electric",
+			usage_priority = "secondary-input",
+		},
+		extension_speed = 1.0,
+		rotation_speed = 1.0,
+		collision_box = {{-0.1, -0.1}, {0.1, 0.1}},
+		selection_box = {{-0.0, -0.0}, {0.0, 0.0}},
+		pickup_position = {0, 0},
+		insert_position = {0, 1.0},
+		--[[
+		platform_picture = { sheet = empty_sheet },
+		hand_base_picture = empty_sheet,
+		hand_open_picture = empty_sheet,
+		hand_closed_picture = empty_sheet,
+		]]
+		platform_picture = data.raw.inserter["inserter"].platform_picture,
+		hand_base_picture = data.raw.inserter["inserter"].hand_base_picture,
+		hand_open_picture = data.raw.inserter["inserter"].hand_open_picture,
+		hand_closed_picture = data.raw.inserter["inserter"].hand_closed_picture,
+	}
+	data.raw.inserter[name] = loader_inserter
+end
+
+local function create_miniloader(base_entity, tech_prereqs)
+	local name = base_entity.name .. "-miniloader"
 	local entity = util.table.deepcopy(base_entity)
 	entity.name = name
-	entity.minable.result = "railloader-" .. base_entity.name
+	entity.minable.result = name
 	entity.max_distance = 0
 	data.raw["underground-belt"][name] = entity
 
@@ -52,7 +74,8 @@ local function create_railloader(base_entity, tech_prereqs)
 		ingredients =
 		{
 			{base_entity.name, 2},
-			{"stack-inserter", 2},
+			{"steel-plate", 8},
+			{"stack-inserter", 4},
 		},
 		result = name
 	}
@@ -76,115 +99,10 @@ local function create_railloader(base_entity, tech_prereqs)
 		order = main_prereq.order
 	}
 	data.raw["technology"][name] = technology
+
+	create_inserter(base_entity)
 end
 
-create_railloader(data.raw["underground-belt"]["underground-belt"], {"stack-inserter"})
-create_railloader(data.raw["underground-belt"]["fast-underground-belt"], {"logistics-2", "railloader-underground-belt"})
-create_railloader(data.raw["underground-belt"]["express-underground-belt"], {"logistics-3", "railloader-fast-underground-belt"})
-
---[[
-data.raw["underground-belt"]["railloader"] = util.table.deepcopy(data.raw["underground-belt"]["underground-belt"])
-data.raw["underground-belt"]["railloader"].name = "railloader"
-data.raw["underground-belt"]["railloader"].minable.result = "railloader"
-data.raw["underground-belt"]["railloader"].max_distance=0
-data.raw.item["railloader"] = util.table.deepcopy(data.raw.item["underground-belt"])
-data.raw.item["railloader"].name = "railloader"
-data.raw.item["railloader"].place_result = "railloader"
-data.raw.recipe["railloader"] = util.table.deepcopy(data.raw.recipe["underground-belt"])
-data.raw.recipe["railloader"].name = "railloader"
-data.raw.recipe["railloader"].result = "railloader"
-data.raw.recipe["railloader"].enabled = true
-data.raw.recipe["railloader"].result_count = nil
-]]
---[[{
-	type = "underground-belt",
-	name = "railloader",
-	icon = "__base__/graphics/icons/underground-belt.png",
-	flags = {"placeable-neutral", "player-creation"},
-	minable = {hardness = 0.2, mining_time = 0.5, result = "underground-belt"},
-	max_health = 150,
-	corpse = "small-remnants",
-	max_distance = 5,
-	underground_sprite =
-	{
-		filename = "__core__/graphics/arrows/underground-lines.png",
-		priority = "high",
-		width = 64,
-		height = 64,
-		x = 64,
-		scale = 0.5
-	},
-	resistances =
-	{
-		{
-			type = "fire",
-			percent = 60
-		},
-		{
-			type = "impact",
-			percent = 30
-		}
-	},
-	collision_box = {{-0.4, -0.4}, {0.4, 0.4}},
-	selection_box = {{-0.5, -0.5}, {0.5, 0.5}},
-	animation_speed_coefficient = 32,
-	belt_horizontal = basic_belt_horizontal,
-	belt_vertical = basic_belt_vertical,
-	ending_top = basic_belt_ending_top,
-	ending_bottom = basic_belt_ending_bottom,
-	ending_side = basic_belt_ending_side,
-	starting_top = basic_belt_starting_top,
-	starting_bottom = basic_belt_starting_bottom,
-	starting_side = basic_belt_starting_side,
-	fast_replaceable_group = "underground-belt",
-	speed = 0.03125,
-	structure =
-	{
-		direction_in =
-		{
-			sheet =
-			{
-				filename = "__base__/graphics/entity/underground-belt/underground-belt-structure.png",
-				priority = "extra-high",
-				shift = {0.25, 0},
-				width = 57,
-				height = 43,
-				y = 43,
-				hr_version =
-				{
-					filename = "__base__/graphics/entity/underground-belt/hr-underground-belt-structure.png",
-					priority = "extra-high",
-					shift = {0.15625, 0.0703125},
-					width = 106,
-					height = 85,
-					y = 85,
-					scale = 0.5
-				}
-			}
-		},
-		direction_out =
-		{
-			sheet =
-			{
-				filename = "__base__/graphics/entity/underground-belt/underground-belt-structure.png",
-				priority = "extra-high",
-				shift = {0.25, 0},
-				width = 57,
-				height = 43,
-				hr_version =
-				{
-					filename = "__base__/graphics/entity/underground-belt/hr-underground-belt-structure.png",
-					priority = "extra-high",
-					shift = {0.15625, 0.0703125},
-					width = 106,
-					height = 85,
-					scale = 0.5
-				}
-
-			}
-
-		}
-	},
-	ending_patch = ending_patch_prototype
-}
-]]
+create_miniloader(data.raw["underground-belt"]["underground-belt"], {"stack-inserter"})
+create_miniloader(data.raw["underground-belt"]["fast-underground-belt"], {"logistics-2", "underground-belt-miniloader"})
+create_miniloader(data.raw["underground-belt"]["express-underground-belt"], {"logistics-3", "fast-underground-belt-miniloader"})
