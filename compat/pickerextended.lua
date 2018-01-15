f3d4ffc32a264828e5aadf3bca4add08653f6d42
mspielberg <9729801+mspielberg@users.noreply.github.com>
2018-01-15 00:12:30 -0800

    rework PickerExtended dolly support

--- compat/pickerextended.lua
+++ compat/pickerextended.lua
@@ -5,12 +5,48 @@ local M = {}
 
 
 local function on_dolly_moved(event)
-  local player = game.players[event.player_index]
   local entity = event.moved_entity
-  if util.is_miniloader_inserter(entity) then
-    entity.teleport(event.start_pos)
-    player.print({"picker-dollies.cant-be-teleported", entity.localised_name})
+  if not util.is_miniloader_inserter(entity) then
+    return
   end
+
+  local old_pos = event.start_pos
+  local new_pos = entity.position
+
+  -- move inserters
+  local partners = entity.surface.find_entities_filtered{
+    position = old_pos,
+    type = "inserter",
+  }
+  for _, ent in ipairs(partners) do
+    ent.teleport(new_pos)
+  end
+
+  local loader = entity.surface.find_entities_filtered{
+    position = old_pos,
+    type = "loader",
+  }[1]
+
+  local new_loader = entity.surface.create_entity{
+    name = loader.name,
+    position = new_pos,
+    direction = loader.direction,
+    force = loader.force,
+    type = loader.loader_type,
+  }
+  new_loader.destructible = false
+
+  -- move items on belt
+  for i=1,2 do
+    local old_tl = loader.get_transport_line(i)
+    local new_tl = new_loader.get_transport_line(i)
+    for j=1, #old_tl do
+      new_tl.insert_at_back(old_tl[j])
+    end
+    old_tl.clear()
+  end
+
+  loader.destroy()
 end
 
 function M.on_load()
