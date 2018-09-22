9c3af6111254791ecb3edb5c7b1a6f042b5842ef
mspielberg <9729801+mspielberg@users.noreply.github.com>
2018-09-21 23:45:04 -0700

    Fix broken image link in README

--- pack.sh
+++ pack.sh
@@ -2,9 +2,25 @@
 
 ZIP='7z'
 
+function canon_name() {
+	dir="$(realpath "$(dirname "$0")")"
+
+	parse_info() {
+		v=`grep \"$1\" "$dir"/info.json | head -1`
+		v="${v%%\",}"
+		v="${v##*\"}"
+		echo $v
+	}
+
+	name=`parse_info name`
+	version=`parse_info version`
+	echo "${name}_${version}"
+}
+
+
 moddir="$(realpath $(dirname "$0"))"
 tmpdir=$(mktemp -d)
-canon_name="$("$moddir/canon_name.sh")"
+canon_name="$(canon_name)"
 zipfile="$PWD/$canon_name.zip"
 ln -nsf "$moddir" "$tmpdir/$canon_name"
 cd "$tmpdir"
@@ -12,6 +28,7 @@ cd "$tmpdir"
 include=$(
 cat <<- END
 	$canon_name/LICENSE
+	$canon_name/changelog.txt
 	$canon_name/info.json
 	$canon_name/*.lua
 	$canon_name/*.md
@@ -19,7 +36,7 @@ END
 )
 
 
-dirs=$(ls -d "$canon_name"/*/ | grep -vF "$canon_name/$canon_name")
+dirs=$(ls -d "$canon_name"/*/ | grep -vF "$canon_name/$canon_name" | grep -vF resources)
 
 echo "Zipping to $zipfile:"
 for f in $include $dirs
