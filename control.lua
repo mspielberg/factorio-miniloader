82877eb7caf013ad9d628dc3694623a149ad5dd0
mspielberg <9729801+mspielberg@users.noreply.github.com>
2018-10-31 14:08:14 -0700

    fix crash in TheBlueprintLab

--- control.lua
+++ control.lua
@@ -239,7 +239,8 @@ local function on_marked_for_deconstruction(ev)
   local entity = ev.entity
   if not (util.is_miniloader(entity) or util.is_miniloader_inserter(entity)) then return end
   for _, ent in ipairs(entity.surface.find_entities_filtered{position=entity.position}) do
-    if util.is_miniloader(ent) or util.is_miniloader_inserter(ent) then
+    -- order_deconstruction() causes event handlers to be fired which may invalidate entities
+    if ent.valid and (util.is_miniloader(ent) or util.is_miniloader_inserter(ent)) then
       if not ent.to_be_deconstructed(ent.force) then
         ent.order_deconstruction(ent.force)
       end
