2e1d14550615db6ba9f897aa90b4cf7ce611bb61
mspielberg <9729801+mspielberg@users.noreply.github.com>
2019-02-12 14:59:40 -0800

    add compatilibity with xander-mod

--- prototypes/recipes.lua
+++ prototypes/recipes.lua
@@ -1,84 +1,133 @@
 require "util"
 
-local ingredients = {
+local ingredient_sets = {
   -- base
   -- 105 I, 27 C
   ["miniloader"] = {
-    {"underground-belt", 1},
-    {"steel-plate", 8},
-    {"fast-inserter", 6},
+    {
+      {"underground-belt", 1},
+      {"forging-steel", 2},
+      {"mechanism-1", 1},
+    },
+    {
+      {"underground-belt", 1},
+      {"steel-plate", 8},
+      {"fast-inserter", 6},
+    },
   },
   -- 358 I, 128 C, 89 O
   ["fast-miniloader"] = {
-    {"miniloader", 1},
-    {"fast-underground-belt", 1},
-    {"stack-inserter", 4},
+    {
+      {"miniloader", 1},
+      {"fast-underground-belt", 1},
+      {"gear-2", 2},
+    },
+    {
+      {"miniloader", 1},
+      {"fast-underground-belt", 1},
+      {"stack-inserter", 4},
+    },
   },
   -- 628 I, 384 C, 174 O
   ["express-miniloader"] = {
-    {"fast-miniloader", 1},
-    {"express-underground-belt", 1},
-    {"stack-inserter", 2},
+    {
+      {"expedited-miniloader", 1},
+      {"express-underground-belt", 1},
+      {"forging-stainless", 2},
+    },
+    {
+      {"fast-miniloader", 1},
+      {"express-underground-belt", 1},
+      {"stack-inserter", 2},
+    },
   },
 
   -- boblogistics
   ["turbo-miniloader"] = {
-    {"express-miniloader", 1},
-    {"turbo-underground-belt", 1},
-    {"express-stack-inserter", 4},
+    {
+      {"express-miniloader", 1},
+      {"turbo-underground-belt", 1},
+      {"express-stack-inserter", 4},
+    },
   },
   ["ultimate-miniloader"] = {
-    {"turbo-miniloader", 1},
-    {"ultimate-underground-belt", 1},
-    {"express-stack-inserter", 2},
+    {
+      {"turbo-miniloader", 1},
+      {"ultimate-underground-belt", 1},
+      {"express-stack-inserter", 2},
+    },
   },
 
   -- FactorioExtended-Plus-Transport
   ["rapid-mk1-miniloader"] = {
-    {"express-miniloader", 1},
-    {"rapid-transport-belt-to-ground-mk1", 1},
-    {"stack-inserter-mk2", 4},
+    {
+      {"express-miniloader", 1},
+      {"rapid-transport-belt-to-ground-mk1", 1},
+      {"stack-inserter-mk2", 4},
+    },
   },
   ["rapid-mk2-miniloader"] = {
-    {"rapid-mk1-miniloader", 1},
-    {"rapid-transport-belt-to-ground-mk2", 1},
-    {"stack-inserter-mk2", 2},
+    {
+      {"rapid-mk1-miniloader", 1},
+      {"rapid-transport-belt-to-ground-mk2", 1},
+      {"stack-inserter-mk2", 2},
+    },
   },
 
   -- UltimateBelts
   ["ub-ultra-fast-miniloader"] = {
-    {"express-miniloader", 1},
-    {"ultra-fast-underground-belt", 1},
-    {"stack-inserter", 6},
+    {
+      {"express-miniloader", 1},
+      {"ultra-fast-underground-belt", 1},
+      {"stack-inserter", 6},
+    },
   },
   ["ub-extreme-fast-miniloader"] = {
-    {"ub-ultra-fast-miniloader", 1},
-    {"extreme-fast-underground-belt", 1},
-    {"stack-inserter", 6},
+    {
+      {"ub-ultra-fast-miniloader", 1},
+      {"extreme-fast-underground-belt", 1},
+      {"stack-inserter", 6},
+    },
   },
   ["ub-ultra-express-miniloader"] = {
-    {"ub-extreme-fast-miniloader", 1},
-    {"ultra-express-underground-belt", 1},
-    {"stack-inserter", 6},
+    {
+      {"ub-extreme-fast-miniloader", 1},
+      {"ultra-express-underground-belt", 1},
+      {"stack-inserter", 6},
+    },
   },
   ["ub-extreme-express-miniloader"] = {
-    {"ub-ultra-express-miniloader", 1},
-    {"extreme-express-underground-belt", 1},
-    {"stack-inserter", 6},
+    {
+      {"ub-ultra-express-miniloader", 1},
+      {"extreme-express-underground-belt", 1},
+      {"stack-inserter", 6},
+    },
   },
   ["ub-ultimate-miniloader"] = {
-    {"ub-extreme-express-miniloader", 1},
-    {"original-ultimate-underground-belt", 1},
-    {"stack-inserter", 6},
+    {
+      {"ub-extreme-express-miniloader", 1},
+      {"original-ultimate-underground-belt", 1},
+      {"stack-inserter", 6},
+    },
+  },
+
+  -- xander-mod
+  ["expedited-miniloader"] = {
+    {
+      {"fast-miniloader", 1},
+      {"expedited-underground-belt", 1},
+      {"mechanism-2", 1},
+    },
   },
 }
 
 if data.raw["inserter"]["turbo-inserter"] then
-  ingredients["miniloader"][3] = {"inserter", 8}
-  ingredients["fast-miniloader"][3] = {"long-handed-inserter", 8}
-  ingredients["express-miniloader"][3] = {"fast-inserter", 6}
-  ingredients["turbo-miniloader"][3] = {"turbo-inserter", 6}
-  ingredients["ultimate-miniloader"][3] = {"express-inserter", 6}
+  -- boblogistics inserter overhaul support
+  ingredient_sets["miniloader"][3] = {"inserter", 8}
+  ingredient_sets["fast-miniloader"][3] = {"long-handed-inserter", 8}
+  ingredient_sets["express-miniloader"][3] = {"fast-inserter", 6}
+  ingredient_sets["turbo-miniloader"][3] = {"turbo-inserter", 6}
+  ingredient_sets["ultimate-miniloader"][3] = {"express-inserter", 6}
 end
 
 local previous_miniloader = {
@@ -99,7 +148,14 @@ local previous_miniloader = {
   ["ub-ultra-express-"] = "ub-extreme-fast-",
   ["ub-extreme-express-"] = "ub-ultra-express-",
   ["ub-ultimate-"] = "ub-extreme-express-",
+
+  -- xander-mod
+  ["expedited-"] = "fast-",
 }
+
+if data.raw.item["expedited-transport-belt"] then
+  previous_miniloader["express-"] = "expedited-"
+end
 
 local filter_inserters = {
   ["fast-inserter"] = "filter-inserter",
@@ -116,6 +172,22 @@ local filter_inserters = {
   ["stack-inserter-mk2"] = "stack-filter-inserter-mk2",
 }
 
+local function select_ingredient_set(sets)
+  for _, set in ipairs(sets) do
+    local valid = true
+    for _, ingredient in pairs(set) do
+      if valid
+      and not ingredient[1]:find("miniloader")
+      and not data.raw.item[ingredient[1]] then
+        valid = false
+      end
+    end
+    if valid then
+      return set
+    end
+  end
+end
+
 local function create_recipes(prefix)
   local name = prefix .. "miniloader"
   local filter_name = prefix .. "filter-miniloader"
@@ -125,7 +197,7 @@ local function create_recipes(prefix)
     name = name,
     enabled = false,
     energy_required = 1,
-    ingredients = ingredients[name],
+    ingredients = select_ingredient_set(ingredient_sets[name]),
     result = name,
   }
 
@@ -134,7 +206,9 @@ local function create_recipes(prefix)
   if previous_miniloader[prefix] then
     filter_recipe.ingredients[1][1] = previous_miniloader[prefix] .. "filter-miniloader"
   end
-  filter_recipe.ingredients[3][1] = filter_inserters[recipe.ingredients[3][1]]
+  if filter_inserters[recipe.ingredients[3][1]] then
+    filter_recipe.ingredients[3][1] = filter_inserters[recipe.ingredients[3][1]]
+  end
   filter_recipe.result = filter_name
 
   data:extend{
