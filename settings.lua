874ce5d77ba17476902ebcd590fdb37f60412bec
mspielberg <9729801+mspielberg@users.noreply.github.com>
2019-03-31 09:49:31 -0700

    add option for miniloader energy usage

--- settings.lua
+++ settings.lua
@@ -1,6 +1,14 @@
 data:extend{
   {
     type = "bool-setting",
+    name = "miniloader-energy-usage",
+    setting_type = "startup",
+    default_value = true,
+    order = "miniloader-energy-usage",
+  },
+
+  {
+    type = "bool-setting",
     name = "miniloader-snapping",
     setting_type = "runtime-global",
     default_value = true,
