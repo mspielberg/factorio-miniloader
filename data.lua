7e6055e2b5c144911ab0835c612bc974a2b72f2d
mspielberg <9729801+mspielberg@users.noreply.github.com>
2019-08-20 15:53:43 -0700

    Add chute

--- data.lua
+++ data.lua
@@ -16,3 +16,19 @@ end
 for prefix, args in pairs(templates) do
   create_miniloader(prefix, args.next_prefix, args.prerequisite_techs, args.tint, args.base_underground_name)
 end
+
+-- chute
+create_miniloader("chute-", "", {"logistics"}, {r=0.5,g=0.5,b=0.5}, "underground-belt")
+
+data.raw.technology["chute-miniloader"] = nil
+data.raw.recipe["chute-miniloader"].enabled = true
+data.raw.loader["chute-miniloader-loader"].speed = data.raw.loader["chute-miniloader-loader"].speed / 4
+data.raw.inserter["chute-miniloader-inserter"].rotation_speed = data.raw.inserter["chute-miniloader-inserter"].rotation_speed / 4
+data.raw.inserter["chute-miniloader-inserter"].energy_source = {type="void"}
+data.raw.inserter["chute-miniloader-inserter"].energy_per_movement = ".0000001J"
+data.raw.inserter["chute-miniloader-inserter"].energy_per_rotation = ".0000001J"
+
+data.raw.item["chute-filter-miniloader"] = nil
+data.raw.recipe["chute-filter-miniloader"] = nil
+data.raw.loader["chute-filter-miniloader-loader"] = nil
+data.raw.inserter["chute-filter-miniloader-inserter"] = nil
